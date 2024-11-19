7008ac57820035c6dc1536710d473135ed4851f4
dev2 <dev2@gmail.com>
2024-11-19 11:12:53 +0530

    Add project files.

---
+++ Prashanth_mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Prashanth_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
